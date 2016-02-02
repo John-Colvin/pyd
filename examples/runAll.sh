9e44fcb93b3c04dcb2747a1d952b0b48f6a7ceaa
John Colvin <john.loughran.colvin@gmail.com>
2016-02-02 15:54:41 +0000

    support embedding

--- examples/runAll.sh
+++ examples/runAll.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-examples="arraytest
+extend_examples="arraytest
 extend_type_conversion
 inherit
 testdll
@@ -10,12 +10,27 @@ def_example
 hello
 wrap"
 
-for folder in $examples; do
+embed_examples="pyind
+simple_embedded
+interpcontext
+pydobject"
+
+for folder in $extend_examples; do
     pushd $folder
     $1 ../../genDubConfig.py $folder > dub.json
     [[ $# = 1 ]] && dub build -v
     [[ $# = 2 ]] && dub build -v --compiler=$2
     [[ $# = 3 ]] && dub build -v --compiler=$2 --arch=$3
     $1 test.py
+    popd
+done
+
+for folder in $embed_examples; do
+    pushd $folder
+    $1 ../../genDubConfig.py $folder embed > dub.json
+    [[ $# = 1 ]] && dub build -v
+    [[ $# = 2 ]] && dub build -v --compiler=$2
+    [[ $# = 3 ]] && dub build -v --compiler=$2 --arch=$3
+    ./$folder
     popd
 done
--- genDubConfig.py
+++ genDubConfig.py
@@ -1,14 +1,34 @@
 from __future__ import print_function
 from distutils import sysconfig
 import sys
+import getopt
 
-if len(sys.argv) < 2:
-    print("Error: Must provide a name as first argument, "
-            "e.g. \"python3 ./genDubConfig.py myModule\"",
-            file=sys.stderr)
+help = '''Creates a dub.json for a project depending
+on pyd.
+
+Usage:
+Run with the python interpreter you want to build for.
+Pass the desired name of the package as the first arg
+and optionally pass "embed" if you want to embed
+python in D instead of the default, which is to build
+a extension module for python.
+
+E.g.
+
+"python ./genDubConfig.py myModuleName embed"
+
+The output is printed to stdout, so you may want to
+pipe it straight to a file e.g.
+
+"python ./genDubConfig.py myModuleName > dub.json"
+'''
+
+if len(sys.argv) < 2 or (len(sys.argv) > 2 and sys.argv[2] != 'embed'):
+    print(help, file=sys.stderr)
     exit(1)
 
 name = sys.argv[1]
+embed = len(sys.argv) > 2 
 vinfo = sys.version_info
 py2 = vinfo[0] == 2
 py3 = vinfo[0] == 3
@@ -19,15 +39,15 @@ print(
 '''{
     "name": "''' + name + '''",
     "sourcePaths": ["."],
-    "targetType": "dynamicLibrary",
+    "targetType": ''' + ('''"executable"''' if embed else '''"dynamicLibrary"''') + ''',
     "targetName": "''' + name + '''",
-
-    "versions": ["PydPythonExtension"],
+''' + ('' if embed else '''
+    "versions": ["PydPythonExtension"],''') + '''
     "dependencies": { "pyd": { "path": "../../" } },
     "subConfigurations": { "pyd": "python''' + str(vinfo[0]) + str(vinfo[1]) + '''" },
     "lflags-posix": ["-L''' + libdir + '''"],
-    "libs": ["''' + libname + '''"],
+    "libs": ["''' + libname + '''"]''' + ('' if embed else ''',
     "postBuildCommands-posix": [
         "mv lib$$DUB_TARGET_NAME.so $$DUB_TARGET_NAME.so"
-    ]
+    ]''') + '''
 }''')
